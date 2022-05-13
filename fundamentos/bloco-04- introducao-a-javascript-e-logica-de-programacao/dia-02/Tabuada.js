fa61a53984f60dfd8b20e1d1cffd702567b8af77
Allan <almeidaa405@gmail.com>
2022-05-13 18:13:01 -0400

    Tabuada.js

---
+++ fundamentos/bloco-04- introducao-a-javascript-e-logica-de-programacao/dia-02/Tabuada.js
@@ -0,0 +1,6 @@
+
+for(let number = 1; number <=10; number+=1){
+    console.log('Tabuada do', number, ':');
+    for(let contador = 1; contador <=10; contador +=1)
+    console.log(number*contador)
+}
