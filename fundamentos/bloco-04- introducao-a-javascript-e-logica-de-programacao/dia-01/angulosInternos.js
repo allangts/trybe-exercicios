0dfb3d4f11283d7b62470759e8eaed4622f9fcb0
Allan Almeida <almeidaa405@gmail.com>
2022-05-24 12:20:47 -0400

    exercicio

--- fundamentos/bloco-04- introducao-a-javascript-e-logica-de-programacao/dia-01/angulosInternos.js
+++ fundamentos/bloco-04- introducao-a-javascript-e-logica-de-programacao/dia-01/angulosInternos.js
@@ -1,12 +1,10 @@
-const numberA = 60;
-const numberB = 60;
-const numberC = 60;
-
-
-if(numberA >= 0 && numberB >= 0 && numberC >= 0 && numberA + numberB + numberC == 180){
-    console.log(true);
-}
-else{
-    console.log(false);
-    console.log('A soma dos ângulos não teve 180 como resultado ou algum ângulo recebeu um valor negativo.');
+function triangulo(angX, angY,angZ) {
+    let graus;
+    if (angX + angY + angZ === 180) {
+    graus = true;
+    }
+    else {
+        graus = false
+    }
+    return graus;
 }
