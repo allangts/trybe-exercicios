627c5e8ce3ff669c39fe6501819e03b65baa99f3
Allan Almeida <almeidaa405@gmail.com>
2022-05-24 13:05:08 -0400

    exercicios

--- fundamentos/bloco-04- introducao-a-javascript-e-logica-de-programacao/dia-02/mediaAritmetica.js
+++ fundamentos/bloco-04- introducao-a-javascript-e-logica-de-programacao/dia-02/mediaAritmetica.js
@@ -3,7 +3,7 @@ function somar(numbers) {
     for(let index = 0; index < numbers.length; index +=1) {
     soma += numbers[index];
     }
-return soma;
+return soma / numbers.length;
 }
 
 console.log(somar([5, 9, 3, 19, 70, 8, 100, 2, 35, 27]));
